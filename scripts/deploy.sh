190b6c8feca872c7e883384457aea9bfdd9dfaf4
Dan Bate <dan@wildfish.com>
2017-03-24 17:26:50 +0000

    added extra config to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,13 @@ set -e
 
 IMAGE_NAME=eu.gcr.io/wildfish-directory/gckb-example
 IMAGE_TAG=${IMAGE_NAME}:$1
+CLUSTER_NAME=gckb-example
+COMP_ZONE=europe-west1-d
 
 gcloud docker -- tag $IMAGE_NAME $IMAGE_TAG
 gcloud docker -- push $IMAGE_TAG
 
-gcloud --quiet container clusters get-credentials gckp-example
+gcloud --quiet config set config set container/cluster ${CLUSTER_NAME}
+gcloud --quiet config set compute/zone ${COMP_ZONE}
+gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 kubectl -- patch deployment django -p'{"spec":{"template":{"spec":{"containers":[{"name":"django","image":"${IMAGE_TAG}"}]}}}}'
