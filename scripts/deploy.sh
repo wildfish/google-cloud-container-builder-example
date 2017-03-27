414b44d3c9a66e73c7f634cb3f4f8de09c7dc59a
Dan Bate <dan@wildfish.com>
2017-03-27 09:39:48 +0100

    removed getting credentials from the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,5 +13,4 @@ gcloud docker -- push $IMAGE_TAG
 
 gcloud --quiet config set container/cluster ${CLUSTER_NAME}
 gcloud --quiet config set compute/zone ${COMP_ZONE}
-gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 kubectl -- patch deployment django -p'{"spec":{"template":{"spec":{"containers":[{"name":"django","image":"${IMAGE_TAG}"}]}}}}'
