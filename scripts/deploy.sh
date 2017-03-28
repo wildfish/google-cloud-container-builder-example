341eefebba54345437f3697312ffefe220f8b563
Dan Bate <dan@wildfish.com>
2017-03-28 16:08:23 +0100

    added setting project name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ cd `dirname $0`/..
 
 set -e
 
-IMAGE_NAME=eu.gcr.io/wildfish-directory/gckb-example
+IMAGE_NAME=eu.gcr.io/${PROJECT_ID}/gckb-example
 IMAGE_TAG=${IMAGE_NAME}:$1
 CLUSTER_NAME=gckb-example
 COMP_ZONE=europe-west1-d
@@ -11,7 +11,13 @@ COMP_ZONE=europe-west1-d
 gcloud docker -- tag $IMAGE_NAME $IMAGE_TAG
 gcloud docker -- push $IMAGE_TAG
 
+gcloud config list
+
+gcloud --quiet config set project ${PROJECT_ID}
 gcloud --quiet config set container/cluster ${CLUSTER_NAME}
 gcloud --quiet config set compute/zone ${COMP_ZONE}
+
+gcloud config list
+
 gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 kubectl patch deployment django -p'{"spec":{"template":{"spec":{"containers":[{"name":"django","image":"${IMAGE_TAG}"}]}}}}'
