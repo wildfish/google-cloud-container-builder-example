0aaf951149f22c3ff0338e256accea1dbc551107
Dan Bate <dan@wildfish.com>
2017-03-28 18:03:42 +0100

    changed service account for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,15 @@ COMP_ZONE=europe-west1-d
 gcloud docker -- tag $IMAGE_NAME $IMAGE_TAG
 gcloud docker -- push $IMAGE_TAG
 
-# currently accessing k8s is not possible due to secrets handling
-# for api scopes but when available deploying to gke will look something
-# like this
-#
-# gcloud --quiet config set project ${PROJECT_ID}
-# gcloud --quiet config set container/cluster ${CLUSTER_NAME}
-# gcloud --quiet config set compute/zone ${COMP_ZONE}
-# gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
-# kubectl patch deployment django -p'{"spec":{"template":{"spec":{"containers":[{"name":"django","image":"${IMAGE_TAG}"}]}}}}'
+# currently accessing k8s is not possible out of the box due to secrets handling
+# this pulls credentials from a private google bucket and uses them to deploy
+# the new image in favour of the base service account credentials
+
+gsutil cp gs://${CREDS_BUCKET_NAME}/creds.json /tmp
+gcloud --quiet auth activate-service-account --key-file=/tmp/creds.json
+
+gcloud --quiet config set project ${PROJECT_ID}
+gcloud --quiet config set container/cluster ${CLUSTER_NAME}
+gcloud --quiet config set compute/zone ${COMP_ZONE}
+gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
+kubectl patch deployment django -p'{"spec":{"template":{"spec":{"containers":[{"name":"django","image":"${IMAGE_TAG}"}]}}}}'
